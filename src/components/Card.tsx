142c2e75f7e57fdf4b19b83975aa53745452a38c
Success625 <adeniyioluwaseun625@gmail.com>
2024-12-18 16:42:47 +0100

    #

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,41 +1,43 @@
-import { MouseEventHandler, useState } from "react";
+import { useState } from "react";
 import { FaCheck, FaCopy } from "react-icons/fa";
 
-interface props {
+interface Props {
   text: string;
 }
 
-const Card = ({ text }: props) => {
+const Card = ({ text }: Props) => {
   const [showAll, setShowAll] = useState(false);
-  const [copyTimeout, setCopyTimeout] = useState(0);
+  const [copied, setCopied] = useState(false);
 
   const handleClick = () => {
     setShowAll((prev) => !prev);
   };
 
-  const handleCopyClick: MouseEventHandler<HTMLDivElement> = (e) => {
-    e.stopPropagation();
+  const handleCopyClick = async (e: React.MouseEvent) => {
+    e.stopPropagation(); // Prevent card click event from firing
 
-    clearTimeout(copyTimeout);
-    setCopyTimeout(
-      setTimeout(() => {
-        setCopyTimeout(0);
-      }, 3000)
-    );
+    try {
+      // Copy text to clipboard
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
 
-    navigator.clipboard.writeText(text);
+      // Reset the 'copied' state after 3 seconds
+      setTimeout(() => setCopied(false), 3000);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
   };
 
   return (
     <div className="card" onClick={handleClick}>
       <div
-        className="absolute top-2 right-2 h-auto w-fit"
+        className="absolute top-2 right-2 h-auto w-fit cursor-pointer"
         onClick={handleCopyClick}
       >
-        {copyTimeout ? (
-          <FaCheck />
+        {copied ? (
+          <FaCheck className="text-green-500" />
         ) : (
-          <FaCopy className="text-sm text-slate-400 hover:text-slate-200 transition cursor-pointer" />
+          <FaCopy className="text-sm text-slate-400 hover:text-slate-200 transition" />
         )}
       </div>
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,14 @@ const Navbar: React.FC = () => {
     <nav className="fixed top-0 left-0 w-full p-2 bg-gray-900 text-slate-100 flex justify-between z-50">
       <Link to="/clipodia">
         {/* <img
-          src={`${process.env.PUBLIC_URL}/logo.png`}
+          src={`/logo.png`}
           width={48}
           height={48}
           alt="logo"
           className="rounded-full inline-block mr-2"
         /> */}
         <img
-          src={`logo.png`}
+          src={`${import.meta.env.BASE_URL}/logo.png`}
           width={48}
           height={48}
           alt="logo"
